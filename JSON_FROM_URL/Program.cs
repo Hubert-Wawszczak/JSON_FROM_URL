866b237ad3f391e5e4b851708989f576e3f9eca3
Hubert Wawszczak <hubert.t.wawszczak@gmail.com>
2022-07-11 20:50:15 +0200

    Initial commit

---
+++ JSON_FROM_URL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_FROM_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
